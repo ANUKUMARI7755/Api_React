775d1a110bc9ab50183f5e9eb29b4397e1125fb3
Anukumari <anumourya7755@gmail.com>
2026-01-06 17:10:40 +0530

    Update OtpVerifying.jsx

--- src/components/OtpVerifying.jsx
+++ src/components/OtpVerifying.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 function OtpVerifying() {
   return (
-    //=====================
     <div className='min-h-screen flex justify-center items-center bg-gray-100'>
         <form className='bg-white p-6 rounded shadow-md w-80'>
             <h2 className='text-xl font-semibold mb-4'>Enter OTP</h2>
