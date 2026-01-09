0ca1c4bc75b7d6e20ef4a6bf68faf6c0e2e53a2f
ANUKUMARI7755 <anumourya7755@gmail.com>
2026-01-09 18:31:10 +0530

    update

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,20 +1,39 @@
-
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import product from '../components/images/Product.jpg';
 
 function Dashboard() {
-    return (
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-        <div>
-            <div>
-                <h1>All Products</h1>
+  useEffect(() => {
+    axios.get("http://13.203.212.97:3000/products")
+      .then((res) => {
+        console.log("API Response:", res.data);
+        setData(res.data.data || []);
+        
+      })
+      .catch((error) => {
+        console.error(error);
+        
+      });
+  }, []);
 
-                <di>
-                    <p>Wikipedia is a free online encyclopedia, created and edited by volunteers around the world and hosted by the Wikimedia Foundation.</p>
-                </di>
-            </div>
+  return (
+    <div>
+      <h1>All Products</h1>
+
+
+      {data.map((item) => (
+        <div key={item.id}>
+   
+           <p><b>Name:</b> {item.name}</p>
+           <p><b>Price:</b> ₹{item.unit_price}</p>
+           <p><b>Stock:</b> {item.current_stock}</p>
         </div>
-
-
-    )
+      ))}
+    </div>
+  );
 }
 
 export default Dashboard;
